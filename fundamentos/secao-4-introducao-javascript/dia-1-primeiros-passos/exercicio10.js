7539a9a9f61d2baff224da5160b1dc3e9a5dc1c9
Phelipe <phelipe.copanski@gmail.com>
2022-11-25 21:16:58 -0300

    resolve exercicio 10

---
+++ fundamentos/secao-4-introducao-javascript/dia-1-primeiros-passos/exercicio10.js
@@ -0,0 +1,11 @@
+const costOfProduct = 20;
+const saleValue = -50;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
+
