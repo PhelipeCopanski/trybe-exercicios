260b1f36a7da6c4cf2c65ebae9ea9b36b0f420e4
Phelipe <phelipe.copanski@gmail.com>
2023-01-19 21:50:02 -0300

    resolve exercicio2

--- fundamentos/secao-7-introducao-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/myRemove.test.js
+++ fundamentos/secao-7-introducao-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/myRemove.test.js
@@ -5,7 +5,7 @@ describe('Testar a função myRemove', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     });
 
-    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3 ,4]);
     });
 
