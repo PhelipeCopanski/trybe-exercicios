57920755807751c8fc609f89b1fb4388922122cc
Phelipe <phelipe.copanski@gmail.com>
2023-01-31 23:20:55 -0300

    resolve exercicios do dia

--- fundamentos/secao-8-higher-order-functions-do-javascrip-es6/dia-5-javascrip-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/fixar.js
+++ fundamentos/secao-8-higher-order-functions-do-javascrip-es6/dia-5-javascrip-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/fixar.js
@@ -1,9 +1,53 @@
 const primeNumbers = [17, 23, 37]
-
+const [desessete, vinteETres, trintaESete] = primeNumbers
 const sum = (a, b) => {
-  console.log(a + b);
+  // console.log(a + b);
 }
 
-sum(primeNumbers[0], primeNumbers[2]) // 54
+sum(desessete, trintaESete) // 54
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, animal, bebida] = [bebida, comida, animal]; 
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[,,, ...numerosPares] = numerosPares;
+
+// console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+
+const getNationality = ({ firstName, nationality = 'Brazilian'}) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+// console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 1) => number * value;
+
+console.log(multiply(8));
